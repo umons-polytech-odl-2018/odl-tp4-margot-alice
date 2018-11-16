1a74da513cfcdf8fa89ab3fb1731dd0257e82676
172674 <Alice.TONDEUR@student.umons.ac.be>
2018-11-16 14:27:44 +0100

    TP4 exo1

--- src/main/java/exercise1/ExceptionCatcher.java
+++ src/main/java/exercise1/ExceptionCatcher.java
@@ -8,6 +8,14 @@ public class ExceptionCatcher {
 	}
 
 	void executeExceptionThrowerSafely(int value) {
-		exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+
+		try
+		{
+			exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+		}
+		catch(RuntimeException e){
+			System.out.println("Erreur ! 42");
+		}
+
 	}
 }
